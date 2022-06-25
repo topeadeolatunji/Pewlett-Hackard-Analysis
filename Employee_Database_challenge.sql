2e50a004d1d4449937ea2bbc3aba876ff1342ef1
topeadeolatunji <104689265+topeadeolatunji@users.noreply.github.com>
2022-06-25 11:27:03 -0400

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -43,3 +43,22 @@ ON ut.emp_no = t.emp_no
 GROUP BY t.title
 ORDER BY count DESC;
 ORDER BY count DESC;
+
+-- A query to create employees memtorship eligibility
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO memtorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC;
