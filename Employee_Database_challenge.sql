52dee66abd29b468a3f96cfa8abf0efeceb9f394
topeadeolatunji <104689265+topeadeolatunji@users.noreply.github.com>
2022-06-25 01:51:27 -0400

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -11,3 +11,26 @@ INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no DESC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+    e.first_name,
+e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO unique_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (t.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC;
+
+SELECT COUNT(ut.emp_no), t.title
+FROM unique_titles as ut
+LEFT JOIN titles as t
+ON ut.emp_no = t.emp_no
+GROUP BY t.title
+ORDER BY count DESC;
